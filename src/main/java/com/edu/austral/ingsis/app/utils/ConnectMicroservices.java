2197a69e9f943c5c8bc02bf6582cd32475857519
Rochi Ferreiro <43883272+rocioferreiro@users.noreply.github.com>
2021-06-18 15:44:10 -0300

    Update ConnectMicroservices.java

--- src/main/java/com/edu/austral/ingsis/app/utils/ConnectMicroservices.java
+++ src/main/java/com/edu/austral/ingsis/app/utils/ConnectMicroservices.java
@@ -28,7 +28,7 @@ public class ConnectMicroservices {
   }
 
   public static String connectToUserMicroservice(String url, HttpMethod method, String token) {
-    final ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080" + url,
+    final ResponseEntity<String> responseEntity = restTemplate.exchange("http://api-massages:8080" + url,
             method,
             getRequestEntity(token),
             String.class);
